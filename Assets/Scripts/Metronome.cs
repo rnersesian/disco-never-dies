17e23dcdd654fc13fa1031db4523b81c602cb3fa
Kasmyr <nersesian.robert@gmail.com>
2023-07-08 01:40:37 +0200

    Metronome

---
+++ Assets/Scripts/Metronome.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Metronome : MonoBehaviour
+{
+
+    public float bpm;
+    public float maxRadius = 3f;
+    public float radius = 0f;
+
+    public float timer = 0f;
+
+    public SpriteRenderer sr;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sr = gameObject.GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        transform.localScale = Vector3.one * Mathf.Abs(Mathf.Sin(timer/Mathf.PI * 20));
+    }
+}
