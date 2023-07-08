db94e6a35bcb824aaef38934410460a66c3b0ca4
Kasmyr <nersesian.robert@gmail.com>
2023-07-08 10:29:11 +0200

    Basic UI

---
+++ Assets/Scripts/UI/TurnTImerScript.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TurnTImerScript : MonoBehaviour
+
+{
+    public Dancing danceManager;
+
+
+    public Image left;
+    public Image right;
+    public Text turnLabel;
+    
+    public float timerDuration = 2f;
+    public float timer = 0f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timer >= timerDuration) timer -= timerDuration;
+
+        float amount = 1f - timer / timerDuration;
+        left.fillAmount = amount;
+        right.fillAmount = amount;
+        timer += Time.deltaTime;
+
+        if (danceManager.playerTurn)
+        {
+            turnLabel.text = "Your Turn";
+        } else
+        {
+            turnLabel.text = "Rival's Turn";
+        }
+    }
+}
